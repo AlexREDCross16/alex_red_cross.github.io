c62222576649f0d70874005c3f043819bb6a6701
alex <belkill@mail.ru>
2023-06-05 21:27:23 +0300

    first update long tine

--- Redcros16.github.io/js/app.js
+++ Redcros16.github.io/js/app.js
@@ -2,6 +2,13 @@ const logo = document.querySelector('#logo-font-span');
 
 logo.addEventListener('click', (e) => {
     alert('hey')
+
+    copy('123123123123');
 })
 
 console.log(123)
+
+let copy = (textId) =>  {
+    document.getElementById(textId).select();
+    document.execCommand('copy');
+}
