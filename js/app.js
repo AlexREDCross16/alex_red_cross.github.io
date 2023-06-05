eaf532797c5b54264f1205bb8bf2efa550a53c86
alex <belkill@mail.ru>
2023-06-06 00:19:08 +0300

    Revert "update project"
    This reverts commit abf3ab9111bf533cb3bc877c813e8174cb5e5f48.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,7 @@
+const logo = document.querySelector('#logo-font-span');
 
-let now = new Date();
-let firstWorkDate = new Date(2015, 8,1);
-let diff = new Date(now.getTime() - firstWorkDate.getTime());
-let year = diff.getUTCFullYear() - 1970;
-let month = diff.getMonth();
-let day = diff.getDate() - 1;
+logo.addEventListener('click', (e) => {
+    alert('hey')
+})
 
-let itStartDate = new Date(2020, 11,1);
-let diff2 = new Date(now.getTime() - itStartDate.getTime());
-let year2 = diff2.getUTCFullYear() - 1970;
-let month2 = diff2.getMonth();
-let day2 = diff2.getDate() - 1;
-
-let dateExp = document.querySelector('#experience-date');
-dateExp.innerHTML = '(' + year + ' years  ' + month + ' months ' + day + ' days' + ' common experience <br>and ' + year2 + ' years  ' + month2 + ' months ' + day2 + ' days' + ' in IT experience)';
-dateExp.classList.add('position-date');
-
-
-function generatePDF() {
-
-    // Choose the element id which you want to export.
-    const element = document.getElementById('divToExport');
-    // element.style.width = '1920px';
-    // element.style.height = '1200px';
-    const opt = {
-        // margin:       0.1,
-        filename:     'resume.pdf',
-        image:        { type: 'jpeg', quality: 1 },
-        html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait', precision: '12' },
-        pagebreak: { mode: [ 'legacy'] }
-
-    };
-
-    html2pdf().set(opt).from(element).save();
-}
+console.log(123)
