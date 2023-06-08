d7e73646761543c564e21ab23f72a856a2b65666
alex <belkill@mail.ru>
2023-06-08 20:26:56 +0300

    update

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+G
+css/cssprogress.min.css,3/1/31bd5e462eabde8689e852e0f2a0798fce584ea6
+S
+#js/libraries/html2pdf.bundle.min.js,d/0/d0eae94d031880381fd234774222c72392c5ced7
+P
+ js/libraries/jquery.modal.min.js,c/0/c068c1e528155b4d81211cab31b2def5d12d39a9
+P
+ js/libraries/jquery-3.7.0.min.js,f/f/ff50780469f98034cf7642f5a9b2f37b93d30588
+I
+js/libraries/jspdf.min.js,b/6/b6e7a81dbe75bd4c9e2bd13004e8ce939acc177d
--- js/text.js
+++ js/text.js
@@ -1,20 +1,29 @@
-let timePerLetter = 300,
-    newLineCharacter = '|',
-    text = document.createTextNode('');
+// let timePerLetter = 300,
+//     newLineCharacter = '|',
+//     text = document.createTextNode('');
+//
+//
+// document.getElementById('test').appendChild(text);
+//
+// $()
+//
+// function printOut(str) {
+//     let i = 0;
+//     (function main() {
+//         let char = str[i++];
+//         text.nodeValue += char === newLineCharacter ? '\n' : char;
+//         if(i < str.length)
+//             setTimeout(main, timePerLetter);
+//     })();
+// }
 
 
-document.getElementById('test').appendChild(text);
 
-$()
-
-function printOut(str) {
-    let i = 0;
-    (function main() {
-        let char = str[i++];
-        text.nodeValue += char === newLineCharacter ? '\n' : char;
-        if(i < str.length)
-            setTimeout(main, timePerLetter);
-    })();
-}
-
-
+$('.logo-font').hover(function (e) {
+        console.log(e)
+    $(e.target).hide();
+},
+    function (e) {
+        $(e.target).show();
+    }
+    )
